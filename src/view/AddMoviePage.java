8497f4b5d101f022cca400acf23b04691d8f06f1
Sauood-Ansari <sauoodansari01@gmail.com>
2026-04-21 23:25:21 +0530

    Added login,Registration, and Add Movie Page in view

--- src/view/AddMoviePage.java
+++ src/view/AddMoviePage.java
@@ -1,5 +1,70 @@
 package view;
 
-public class AddMoviePage {
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import javax.swing.*;
+import controller.MovieController;
 
+public class AddMoviePage{
+	private JFrame movieCollectionFrame;
+	private JPanel mainPanel;
+	private JTextField title, genre, year, rating;
+	private JButton addMovieButton;
+	
+	String movieTitle,movieGenre;
+	int movieYear;
+	double movieRating;
+
+	MovieController movieController = new MovieController();
+
+	public void addMovie() {
+		movieCollectionFrame =new JFrame("Movies Collection");
+		movieCollectionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		movieCollectionFrame.setSize(400, 400);
+		movieCollectionFrame.setLayout(new GridBagLayout());
+		//movieCollectionFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		title=new JTextField();
+		genre=new JTextField();		
+		year=new JTextField();		
+		rating=new JTextField();
+		
+		mainPanel=new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		
+		mainPanel.add(new JLabel("Movie Name:"));
+		mainPanel.add(title);
+		
+		mainPanel.add(new JLabel("Movie Genre:"));
+		mainPanel.add(genre);
+		
+		mainPanel.add(new JLabel("Release Year:"));
+		mainPanel.add(year);
+		
+		mainPanel.add(new JLabel("Rating Out Of Five"));
+		mainPanel.add(rating);
+		
+		addMovieButton=new JButton("Add Movie");
+		addMovieButton.setBounds(50, 200, 200, 30);	
+		
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		mainPanel.add(addMovieButton);		
+
+		
+		movieCollectionFrame.add(mainPanel);
+
+		addMovieButton.addActionListener(e -> {
+			movieTitle = title.getText();
+			movieGenre = genre.getText();
+			movieYear = Integer.parseInt(year.getText());
+			movieRating = Double.parseDouble(rating.getText());
+
+			movieController.addMovie(movieTitle, movieGenre, movieYear, movieRating);
+			
+		});
+		movieCollectionFrame.setLayout(new GridBagLayout());
+		movieCollectionFrame.setVisible(true);
+
+	}
+	
 }
--- src/view/MovieCollectionPage.java
+++ src/view/MovieCollectionPage.java
@@ -1,37 +1,8 @@
 package view;
 
 import javax.swing.*;
+
 public class MovieCollectionPage 
 {
-    private JPanel mainPanel;
-    private JButton addMovieButton;
-    private JButton viewCollectionButton;
-    private JButton searchMovieButton;
-
-    public MovieCollectionPage() {
-        addMovieButton.addActionListener(e -> {
-            
-            JOptionPane.showMessageDialog(mainPanel, "Add Movie Page opened!");
-        });
-
-        viewCollectionButton.addActionListener(e -> {
-            
-            JOptionPane.showMessageDialog(mainPanel, "View Collection Page opened!");
-        });
-
-        searchMovieButton.addActionListener(e -> {
-            
-            JOptionPane.showMessageDialog(mainPanel, "Search Movie Page opened!");
-        });
-    }
-
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
-
-
-    static void main(String[] args) {
-        
-        
-    }
+	
 }
